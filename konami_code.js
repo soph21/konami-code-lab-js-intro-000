1ed8788d6faf9a620d90c8204c9b5fce3ba467ff
Sophie Shin <sophie@alloy.co>
2021-04-05 19:35:29 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,18 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let i = 0
+  document.body.addEventListener("keydown",(event) =>  {
+debugger
+let key = event.key
+  if (key === codes[i]) {
+    i++
+    if (i === codes.length) {
+      alert('Hurray!')
+      i = 0
+    }
+  } else {
+    i = 0
+  }
+})
 }
